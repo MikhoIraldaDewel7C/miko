f56bd0916eee2d86f90a526c3e542ec22636b86e
Mikho Iralda Dewel <91176690+MikhoIraldaDewel7C@users.noreply.github.com>
2023-01-01 16:33:31 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 $server = "localhost";
-$username = "smpnkahi_skrip";
-$password = "~M)OFD$aV{-.";
+$username = "smpnkahi_user";
+$password = "1208mikodewel"
 $database = "smpnkahi_skrip";
 
 $koneksi = mysqli_connect($server,$username,$password,$database); 
